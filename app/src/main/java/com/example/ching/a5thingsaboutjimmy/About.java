ce64b9c820e0d776bcef073e3ea6532f2d03b4ab
Ching <dino8556@gmail.com>
2016-07-21 22:31:48 +0800

    Fix the button bug

--- app/src/main/java/com/example/ching/a5thingsaboutjimmy/About.java
+++ app/src/main/java/com/example/ching/a5thingsaboutjimmy/About.java
@@ -1,7 +1,9 @@
 package com.example.ching.a5thingsaboutjimmy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    public void ActionGo (View view){
+        Intent myintent = new Intent(About.this,FiveThings.class);
+        startActivity(myintent);
     }
 }
